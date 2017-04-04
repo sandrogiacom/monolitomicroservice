fad9ae36c2cb5e0436fbfed6c451e7802539dd77
Vanei Anderson Heidemann <vanei.heidemann@fluig.com>
2017-04-04 10:47:29 -0300

    Implementaçoes.

--- session/docker/serverejb/startup.sh
+++ session/docker/serverejb/startup.sh
@@ -4,4 +4,4 @@ mkdir -p /opt/session
 cp /opt/session/SessionEJBServer.ear /opt/jboss/wildfly/standalone/deployments/SessionEJBServer.ear
 
 /wait-for-it.sh -h sessionmysql -p 3306 -t 90
-/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -Djboss.node.name=serverejb
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -Djboss.node.name=serverejb --debug
