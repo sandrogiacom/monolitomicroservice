b8f59a9d0f30fe6f3c71361eae1258f315126065
Vanei Anderson Heidemann <vanei.heidemann@fluig.com>
2017-04-28 17:09:04 -0300

    Implementaçoes para setar o token no login-module.

---
+++ wildfly/security/common/src/main/java/com/monolitomicroservice/teste/wildfly/security/common/SecurityConstants.java
@@ -0,0 +1,10 @@
+package com.monolitomicroservice.teste.wildfly.security.common;
+
+public final class SecurityConstants {
+    public static final String JWT_ATTRIBUTE = "_jwt_token_";
+    public static final String LOGGED_ATTRIBUTE = "_logged_";
+    public static final String LOGOUT_REQUIRED_ATTRIBUTE = "_logout_required_";
+
+    private SecurityConstants() {
+    }
+}
